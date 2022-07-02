ce50580a0ca408773c49f439e09d5a2df19867d6
Alican Akca <alican.akca@std.ieu.edu.tr>
2022-07-02 23:41:26 +0300

    Condition has been added.

--- R Course/R/cachematrix.R
+++ R Course/R/cachematrix.R
@@ -1,13 +1,16 @@
-## This function generates a matrix of random numbers.
+## This function generates a matrix of random numbers. But, 
+## It checks if the reverse is received first.
 
 makeCacheMatrix <- function(x = matrix()) {
         x <- matrix(sample(1:100, 9, replace=TRUE), nrow=3)
-        return(x)
+        if (solve(x) !=x) {
+                return(x)
+        }     
 }
 
 
 ## This function returns the inverse of matrix giving by
-## function makeCacheMatrix.
+## function makeCacheMatrix. 
 
 cacheSolve <- function(x, ...) {
         inverseM <- makeCacheMatrix(x)
